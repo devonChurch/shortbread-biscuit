74d4c6fdc556405441e49a4499cc3def4dc942b3
devonChurch <notifydevon@gmail.com>
2019-01-05 22:54:39 +1300

    Updates

---
+++ precache-manifest.e80f57cfe8d4139cf0ad2ece37e03394.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80f8a5cf891706e0049d",
+    "url": "/shortbread-biscuit/static/js/runtime~main.80f8a5cf.js"
+  },
+  {
+    "revision": "a551ff7927665ee197eb",
+    "url": "/shortbread-biscuit/static/js/main.a551ff79.chunk.js"
+  },
+  {
+    "revision": "5a7f85e1293b0d21cae0",
+    "url": "/shortbread-biscuit/static/js/1.5a7f85e1.chunk.js"
+  },
+  {
+    "revision": "5a7f85e1293b0d21cae0",
+    "url": "/shortbread-biscuit/static/css/1.d8230058.chunk.css"
+  },
+  {
+    "revision": "10ddf59b87fc9214957ddea8c22b0f7a",
+    "url": "/shortbread-biscuit/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.63ce76dbc9d0e56e015e6c1de554e62b.js"
+  "/shortbread-biscuit/precache-manifest.e80f57cfe8d4139cf0ad2ece37e03394.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/shortbread-biscuit/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
