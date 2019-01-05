6d87142af8e09317b367f2765da38de63b28058c
devonChurch <notifydevon@gmail.com>
2019-01-05 22:18:38 +1300

    Updates

---
+++ precache-manifest.63ce76dbc9d0e56e015e6c1de554e62b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e68d10ed621db1435bb2",
+    "url": "/static/js/main.e68d10ed.chunk.js"
+  },
+  {
+    "revision": "86da89361ee905254d28",
+    "url": "/static/js/1.86da8936.chunk.js"
+  },
+  {
+    "revision": "86da89361ee905254d28",
+    "url": "/static/css/1.d8230058.chunk.css"
+  },
+  {
+    "revision": "82703199116b16581d978bf0615a5106",
+    "url": "/index.html"
+  }
+];
